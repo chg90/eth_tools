41ab397f478475af7395ce8f35442148b24b5f6f
CaoHuan <371902449@qq.com>
2020-04-24 17:58:44 +0800

    init

---
+++ transaction/send_raw_transaction.go
@@ -0,0 +1,13 @@
+package transaction
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+/*
+
+ */
+func SendRawTransaction(client ethclient.Client, from, to common.Address) {
+
+}
