567f6ce5c3884998378780c37a242c2b64e79388
CaoHuan <371902449@qq.com>
2020-04-29 16:53:11 +0800

    mod

--- mail/sendmail.go
+++ mail/sendmail.go
@@ -23,10 +23,10 @@ func SendMail(mailTo []string, subject string, body string) error {
 	port, _ := strconv.Atoi(mailConn["port"]) //转换端口类型为int
 
 	m := gomail.NewMessage()
-	m.SetHeader("From", "XD Game"+"<"+mailConn["user"]+">") //这种方式可以添加别名，即“XD Game”， 也可以直接用<code>m.SetHeader("From",mailConn["user"])</code> 读者可以自行实验下效果
-	m.SetHeader("To", mailTo...)                            //发送给多个用户
-	m.SetHeader("Subject", subject)                         //设置邮件主题
-	m.SetBody("text/html", body)                            //设置邮件正文
+	m.SetHeader("From", "<"+mailConn["user"]+">") //这种方式可以添加别名，也可以不用别名 即m.SetHeader("From",mailConn["user"])
+	m.SetHeader("To", mailTo...)                  //发送给多个用户
+	m.SetHeader("Subject", subject)               //设置邮件主题
+	m.SetBody("text/html", body)                  //设置邮件正文
 
 	d := gomail.NewDialer(mailConn["host"], port, mailConn["user"], mailConn["pass"])
 
